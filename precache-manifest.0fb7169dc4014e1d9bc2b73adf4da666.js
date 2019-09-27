6141559a39c1e4a759cb48c5d580912227bd9163
quilldata <abhay@dappquery.com>
2019-09-28 01:11:58 +0530

    Updates

---
+++ precache-manifest.0fb7169dc4014e1d9bc2b73adf4da666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938b36f117041eea19f48b678c74883a",
+    "url": "/kleros-subgraph/index.html"
+  },
+  {
+    "revision": "4c1a03cd562c267154e3",
+    "url": "/kleros-subgraph/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "184c4256c387f62b5ee5",
+    "url": "/kleros-subgraph/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "4c1a03cd562c267154e3",
+    "url": "/kleros-subgraph/static/js/2.8455334c.chunk.js"
+  },
+  {
+    "revision": "184c4256c387f62b5ee5",
+    "url": "/kleros-subgraph/static/js/main.62e511ae.chunk.js"
+  },
+  {
+    "revision": "c9bad8eb820ddd31a5c8",
+    "url": "/kleros-subgraph/static/js/runtime-main.71f1a62c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kleros-subgraph/precache-manifest.dd66b7b3997c36b3c8be1358970d8ea9.js"
+  "/kleros-subgraph/precache-manifest.0fb7169dc4014e1d9bc2b73adf4da666.js"
 );
 
 self.addEventListener('message', (event) => {
